4d5291cb22d013faa26f98e7fe7eec4526e4d5e8
Rob McBroom <github@skurfer.com>
2011-12-09 15:13:38 -0500

    removing copyright lines

--- Quicksilver Plug-in.xctemplate/___PACKAGENAMEASIDENTIFIER___Action.h
+++ Quicksilver Plug-in.xctemplate/___PACKAGENAMEASIDENTIFIER___Action.h
@@ -3,7 +3,6 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
 #import <QSCore/QSObject.h>
--- Quicksilver Plug-in.xctemplate/___PACKAGENAMEASIDENTIFIER___Source.m
+++ Quicksilver Plug-in.xctemplate/___PACKAGENAMEASIDENTIFIER___Source.m
@@ -3,7 +3,6 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
 #import "___PROJECTNAMEASIDENTIFIER___Source.h"
